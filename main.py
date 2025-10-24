034ddf123e6dd60b447c6b145e97351ebfaf2488
shawn <shawnhutchins1@gmail.com>
2025-10-23 21:02:17 -0400

    Added tomato image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import PhotoImage
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -18,6 +20,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro?")
+
+canvas = tk.Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
 
 
 window.mainloop()
