c58d2ad78887d73fab1dc00c141e58c379d49aff
shawn <shawnhutchins1@gmail.com>
2025-11-23 20:56:24 -0500

    Removed testing code

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ reps = 0
 def start_timer():
     global reps
     reps += 1
-    print(reps)
-    work_sec = WORK_MIN# * 60
-    short_break_sec = SHORT_BREAK_MIN# * 60
-    long_break_sec = LONG_BREAK_MIN# * 60
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_break_sec)
