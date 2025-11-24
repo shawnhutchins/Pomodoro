e268a133d91e7bbec1d9963612f1c17cf17cfa4d
shawn <shawnhutchins1@gmail.com>
2025-11-23 20:41:48 -0500

    Configured start_timer function to alternate between work and break timers

--- main.py
+++ main.py
@@ -11,12 +11,27 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps < 8:
+        if reps % 2 != 0:
+            count_down(work_sec)
+        else:
+            count_down(short_break_sec)
+    elif reps == 8:
+        count_down(long_break_sec)
+    else:
+        print("Restart the timer please")
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
